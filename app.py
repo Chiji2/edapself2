fda20a0a7e7ccdf66ff73a4b80178bd58e0b8514
Chiji2 <nwosuchijioke@gmail.com>
2025-03-17 21:14:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ if st.button("Check this"):
   plt.title("Random 150-unit Time Series")
   plt.xlabel("Time")
   plt.ylabel("Values")
-  coefficients = np.polyfit("Time" "Values",1)
+  coefficients = np.polyfit("Time", "Values",1)
   trendline = np.poly1d(coefficients)
   st.pyplot(plt.gcf())
