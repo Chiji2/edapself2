af12a89b3fa657b5d08dc73c2fdf2121a36eda62
Chiji2 <nwosuchijioke@gmail.com>
2025-03-17 21:42:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import streamlit as st
 if st.button("Check this"):
-  #time_series = np.random.randn(150)
-  n = 150    #No. of time steps
+    n = 150    #No. of time steps
   x = np.arange(n)    #time steps
   y = np.cumsum(np.random.randn(n))    #Random walk - cumulative sum of normal distribution
   coefficients = np.polyfit(x, y, 1)
@@ -13,8 +12,12 @@ if st.button("Check this"):
   plt.xlabel("Time steps")
   plt.ylabel("Values")
   plt.title("Random walk with trend line")
+  st.pyplot(plt.gcf())
+
+  #Herkulaas' code for the random walk is as follows:
+  #time_series = np.random.randn(150)
   #plt.plot(time_series)
   #plt.title("Random 150-unit Time Series")
   #plt.xlabel("Time")
   #plt.ylabel("Values")
-  st.pyplot(plt.gcf())
+  #st.pyplot(plt.gcf())
