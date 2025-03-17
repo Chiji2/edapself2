c1e3e4d19a500944619046226745a74d64c26808
Chiji2 <nwosuchijioke@gmail.com>
2025-03-17 21:08:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import streamlit as st
 if st.button("Check this"):
-time_series = np.random.randn(150)
-plt.plot(time_series)
-plt.title("Random 150-unit Time Series")
-plt.xlabel("Time")
-plt.ylabel("Values")
-plt.show()
-plt.pyplot(plt.gcf())
+  time_series = np.random.randn(150)
+  plt.plot(time_series)
+  plt.title("Random 150-unit Time Series")
+  plt.xlabel("Time")
+  plt.ylabel("Values")
+  plt.pyplot(plt.gcf())
