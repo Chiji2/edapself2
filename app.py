339720c7959e556d4ee8e6a2edb8ca3abce06b79
Chiji2 <nwosuchijioke@gmail.com>
2025-03-17 21:05:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
-time_series = np.random.randn(100)
+import streamlit as st
+if st.button("Test this one_now I know why it failed before"):
+time_series = np.random.randn(150)
 plt.plot(time_series)
-plt.title("Random 100-unit Time Series")
+plt.title("Random 150-unit Time Series")
 plt.xlabel("Time")
 plt.ylabel("Values")
 plt.show()
+plt.pyplot(plt.gcf())
